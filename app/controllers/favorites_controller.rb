ce3b2e9191f1fb45a552c7e81d9cbc9981eb43bb
taroken65 <yonemurano@gmail.com>
2021-08-06 14:50:59 +0000

    0806 commit2

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,24 +1,18 @@
 class FavoritesController < ApplicationController
  before_action :authenticate_user!
- before_action :book_params
 
  def create
     @book = Book.find(params[:book_id])
     favorite = @book.favorites.new(user_id: current_user.id)
     favorite.save
-    
+    redirect_to request.referer
  end
 
   def destroy
     @book = Book.find(params[:book_id])
     favorite = @book.favorites.find_by(user_id: current_user.id)
     favorite.destroy
-    
-  end
-  
-  private
-  def book_params
-    @book = Book.find(params[:id])
+    redirect_to request.referer
   end
 
 end
